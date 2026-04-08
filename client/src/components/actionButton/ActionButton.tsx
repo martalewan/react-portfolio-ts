4c7ae484ba4fab41f6810545ccebb9a931294731
martalewan <marta.lewan@hotmail.com>
2026-04-08 20:49:26 +0200

    feat: add reusable ActionButton component

--- client/src/components/actionButton/ActionButton.tsx
+++ client/src/components/actionButton/ActionButton.tsx
@@ -6,10 +6,10 @@ type ActionButtonProps = {
 const ActionButton = ({ text }: ActionButtonProps) => {
     return (
         <button className="group relative">
-            <div className="relative z-1 inline-flex h-8 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium text-neutral-600 transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-active:translate-x-0 group-active:translate-y-0">
+            <div className="relative z-1 inline-flex h-8 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium text-neutral-600 transition-all duration-300 group-hover:-translate-x-1 group-hover:-translate-y-1 group-active:translate-x-0 group-active:translate-y-0">
                 {text}
             </div>
-            <div className="absolute inset-0 z-0 h-full w-full rounded-md transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:[box-shadow:5px_5px_#a3a3a3,10px_10px_#d4d4d4,15px_15px_#e5e5e5] group-active:translate-x-0 group-active:translate-y-0 group-active:shadow-none"></div>
+            <div className="absolute inset-0 z-0 h-full w-full rounded-md transition-all duration-300 group-hover:-translate-x-1 group-hover:-translate-y-1 group-hover:[box-shadow:1px_1px_#a3a3a3,2px_2px_#d4d4d4,3px_3px_#e5e5e5] group-active:translate-x-0 group-active:translate-y-0 group-active:shadow-none"></div>
         </button>
     )
 }
