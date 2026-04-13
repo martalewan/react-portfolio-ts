015010e250d9b7853d989e97ffd58d0a6eb6d0a7
martalewan <marta.lewan@hotmail.com>
2026-04-13 18:45:33 +0200

    feat: add new cursor component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import About from "./components/about/About";
+import Cursor from "./components/Cursor";
+import DotsContainer from "./components/dotsContainer/DotsContainer";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Skills from "./components/skills/Skills";
@@ -7,9 +9,11 @@ const App = () => {
   return (
     <div className="bg-(--primary-color) relative bg-[linear-gradient(to_right,rgba(255,255,255,0.05)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.05)_1px,transparent_1px)]
   bg-size-[540px_540px] bg-position-[calc(50%)_calc(50%)]">
+      <Cursor />
       <Navbar />
       <Hero />
       <About />
+      <DotsContainer />
       <Skills />
     </div>
   );
---
+++ client/src/components/Cursor.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+const Cursor = () => {
+    const [pos, setPos] = useState({ x: 0, y: 0 });
+    const [hovered, setHovered] = useState(false);
+
+    useEffect(() => {
+        const move = (e: MouseEvent) => {
+            setPos({ x: e.clientX, y: e.clientY });
+        };
+
+        const handleEnter = () => setHovered(true);
+        const handleLeave = () => setHovered(false);
+
+        window.addEventListener("mousemove", move);
+
+        const addHoverListeners = () => {
+            const hoverables = document.querySelectorAll(
+                "a, button, [data-hover]"
+            );
+
+            hoverables.forEach((el) => {
+                el.addEventListener("mouseenter", handleEnter);
+                el.addEventListener("mouseleave", handleLeave);
+            });
+        };
+
+        addHoverListeners();
+
+        return () => {
+            window.removeEventListener("mousemove", move);
+        };
+    }, []);
+
+    return (
+        <div className="pointer-events-none fixed inset-0 z-12 mix-blend-difference">
+
+            <div
+                className="absolute w-4 h-4 rounded-full bg-white transition-transform duration-300 ease-out"
+                style={{
+                    transform: `
+            translate(${pos.x - 4}px, ${pos.y - 4}px)
+            scale(${hovered ? 5 : 0.5})
+          `,
+                }}
+            />
+        </div>
+    );
+};
+
+export default Cursor;
