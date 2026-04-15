ab12b17f401aec4cf4adf81f7e0a61c6c2cd4e6e
martalewan <marta.lewan@hotmail.com>
2026-04-15 13:13:30 +0200

    feat: add aboutIntro component

---
+++ client/src/components/modules/AboutIntro.tsx
@@ -0,0 +1,34 @@
+const AboutIntro = () => {
+    return (
+        <div className="grid grid-cols-2 gap-22 mb-40">
+            <div>
+                <h3 className="mb-10">Strong frontend foundation</h3>
+                <p className="mt-4 text-(--secondary-color)/90">
+                    I specialize in building scalable, performant web applications using modern technologies like React and TypeScript. I focus on clean architecture, maintainable code, and creating interfaces that are not only visually refined, but also fast, accessible, and reliable in real-world use.
+                </p>
+            </div>
+
+            <div>
+                <h3 className="mb-10">Design-first engineering</h3>
+                <p className="mt-4 text-(--secondary-color)/90">
+                    My background is rooted in graphic design, with over 7 years of experience shaping visual systems, layouts, and user experiences. That foundation still drives how I approach frontend development today. I don’t just build interfaces — I think about structure, hierarchy, and clarity from the very first line of code.
+                </p>
+            </div>
+
+            <div>
+                <h3 className="mb-10">Creative discipline</h3>
+                <p className="mt-4 text-(--secondary-color)/90">
+                    Before tech, I spent years as a cellist. That experience shaped my discipline, patience, and attention to nuance — qualities I bring into my work every day. Whether refining UI details or structuring complex components, I approach problems with focus and precision.
+                </p>
+            </div>
+            <div>
+                <h3 className="mb-10">Curiosity & continuous growth</h3>
+                <p className="mt-4 text-(--secondary-color)/90">
+                    Transitioning from design to frontend wasn’t a switch — it was a deliberate evolution. I enjoy learning, experimenting, and improving with every project. I’m motivated by building better experiences and pushing my craft forward, both technically and creatively.
+                </p>
+            </div>
+
+        </div>)
+}
+
+export default AboutIntro
