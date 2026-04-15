b673c5d9055121efaf1a81386a8593456ba5a7aa
martalewan <marta.lewan@hotmail.com>
2026-04-15 12:22:03 +0200

    feat: add section title component

---
+++ client/src/components/modules/SectionTitle.tsx
@@ -0,0 +1,24 @@
+type SectionTitle = {
+    label: string,
+    title: string,
+    subtitle: string,
+}
+
+const SectionTitle = ({ label, title, subtitle }: SectionTitle) => {
+    return (
+
+        <div className="relative flex flex-col items-start gap-2 pl-10">
+            <div className="absolute left-0 top-1/2 w-6 h-px bg-white/20" />
+
+            <span className="text-xs tracking-widest text-white/30 uppercase">
+                {subtitle}
+            </span>
+            <h2 className="tracking-tight">
+                <span className="neon-flicker numero-font ">{label}</span>
+                {title}
+            </h2>
+        </div>
+    )
+}
+
+export default SectionTitle
