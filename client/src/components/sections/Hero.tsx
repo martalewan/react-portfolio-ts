4e3fd99d4d23c0e9dccb676d7c95dabb896967c1
martalewan <marta.lewan@hotmail.com>
2026-04-16 14:59:46 +0200

    refactor: remove unused components and styles

--- client/src/components/sections/Hero.tsx
+++ client/src/components/sections/Hero.tsx
@@ -1,19 +1,12 @@
 import { FiDownload } from "react-icons/fi";
-import profil from "../../assets/profil.png";
+import portrait from "../../assets/portrait.svg";
 import Button from "../ui/Button";
-import { motion } from "framer-motion";
 import Socials from "../modules/Socials";
 import ScrollButton from "../ui/ScrollButton";
 
 const Hero = () => {
     return (
         <section id="home" className="page-padding min-h-screen">
-            <div className="neon-bg">
-                <div className="blob b1"></div>
-                <div className="blob b2"></div>
-                <div className="blob b3"></div>
-            </div>
-
             <div className="container h-screen z-1">
                 <div className="h-full flex flex-col xl:flex-row justify-center xl:justify-between items-center gap-6">
                     <div className="flex flex-col items-center gap-5 xl:items-start text-center xl:text-left order-2 xl:order-0">
@@ -41,15 +34,9 @@ const Hero = () => {
                             <Socials wrapperStyles="flex gap-10" iconsStyles="flex items-center justify-center border-1 border-white rounded-full p-2 w-9 h-9 hover:text-(--accent-color) hover:border-(--accent-color) hover:transition-all duration-500" />
                         </div>
                     </div>
-                    {/* <div className="relative flex justify-center items-center">
-                        <div className="neon-frame rounded-full p-0 opacity-99">
-                            <motion.img
-                                src={profil}
-                                alt="Portrait"
-                                className="rounded-full max-w-80 relative"
-                            />
-                        </div>
-                    </div> */}
+                    <div className="relative flex justify-center items-center">
+                        <img src={portrait} alt="portrait" className="w-65 invert brightness-0" />
+                    </div>
                 </div>
             </div>
             <ScrollButton direction="down" scrollToId="about" />
