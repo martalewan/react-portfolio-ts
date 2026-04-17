771da2bc9a4cee68b367c2d224f98c5427836950
martalewan <marta.lewan@hotmail.com>
2026-04-17 23:11:27 +0200

    feat: improve contact section

--- client/src/components/sections/Contact.tsx
+++ client/src/components/sections/Contact.tsx
@@ -4,10 +4,12 @@ import GlassCard from "../ui/GlassCard"
 import profil from "../../assets/profil.jpeg";
 import { motion } from "framer-motion";
 import Button from "../ui/Button";
+import Socials from "../modules/Socials";
+import { FiMapPin, FiMail } from "react-icons/fi";
 
 const Contact = () => {
     return (
-        <section id="contact" className="page-padding pb-50">
+        <section id="contact" className="page-padding pb-50 pointer-none">
             <SectionTitle label="04." title="Get in touch with me" subtitle="Hire me and just tell me hello" />
             <NeonFrame>
 
@@ -24,19 +26,19 @@ const Contact = () => {
                             <input
                                 type="text"
                                 placeholder="Your name"
-                                className="w-full px-4 py-3 rounded-sm bg-(--secondary-color)/7 border border-white/10 text-white placeholder-white/40 focus:outline-none focus:border-(--accent-color)"
+                                className="w-full px-4 py-3 rounded-xs bg-(--secondary-color)/7 border border-white/10 text-white placeholder-white/40 focus:outline-none focus:border-(--accent-color)"
                             />
 
                             <input
                                 type="email"
                                 placeholder="Your email"
-                                className="w-full px-4 py-3 rounded-sm bg-(--secondary-color)/7 border border-white/10 text-white placeholder-white/40 focus:outline-none focus:border-(--accent-color)"
+                                className="w-full px-4 py-3 rounded-xs bg-(--secondary-color)/7 border border-white/10 text-white placeholder-white/40 focus:outline-none focus:border-(--accent-color)"
                             />
 
                             <textarea
                                 placeholder="Your message"
                                 rows={4}
-                                className="w-full px-4 py-3 rounded-sm bg-(--secondary-color)/7 border border-white/10 text-white placeholder-white/40 focus:outline-none focus:border-(--accent-color) resize-none"
+                                className="w-full px-4 py-3 rounded-xs bg-(--secondary-color)/7 border border-white/10 text-white placeholder-white/40 focus:outline-none focus:border-(--accent-color) resize-none"
                             />
 
                             <Button
@@ -49,22 +51,34 @@ const Contact = () => {
                         </form>
 
                     </GlassCard>
-                    <div>
-                        <div className="absolute right-0">
+                    <div className="flex justify-end gap-4">
+                        <Socials wrapperStyles="flex flex-col gap-4" iconsStyles="z-10 flex items-center justify-center border-1 border-(--secondary-color)/80 rounded-xs p-2 w-9 h-9" />
+
+                        <div className="flex flex-col gap-8 items-start">
                             <motion.img
                                 src={profil}
                                 alt="Portrait"
-                                className="rounded-sm max-w-80 relative"
+                                className="rounded-sm max-w-80 w-full h-auto object-contain"
                             />
+                            <div className="flex gap-3">
+                                <div className="badge text-[11px] px-3 py-1.5 flex items-center gap-2">
+                                    <FiMapPin className="text-xs text-(--accent-color)" />
+                                    <span>Paris</span>
+                                </div>
+
+                                <div className="badge text-[11px] px-3 py-1.5 flex items-center gap-2">
+                                    <FiMail className="text-sm text-(--accent-color)" />
+                                    <span>mlewan.design@gmail.com</span>
+                                </div>
+                            </div>
                         </div>
-
-
-
                     </div>
+
 
                 </div>
 
             </NeonFrame>
+
         </section>
     )
 }
