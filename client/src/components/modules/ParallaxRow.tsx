3e5b20b79cec14cb2ba297577df0c2f96614ba4a
martalewan <marta.lewan@hotmail.com>
2026-04-22 04:52:14 +0200

    refactor: improve parallaxRow component

--- client/src/components/modules/ParallaxRow.tsx
+++ client/src/components/modules/ParallaxRow.tsx
@@ -1,13 +1,14 @@
 import { motion } from "framer-motion";
-import { useHorizontalParallax } from "../../animations/parallax";
+import { useHorizontalParallax } from "../../animations";
 
 type PhraseProps = {
     text: string;
     className?: string;
 };
+
 type ParallaxRowProps = {
     progress,
-    direction: "left" | "right",
+    index: number,
     text: string;
     left?: number | string;
 };
@@ -16,10 +17,13 @@ const Phrase = ({ text, className = "" }: PhraseProps) => {
     return (
         <span
             className={`
-                inline-flex items-center
+                inline-flex 
+                items-center
                 whitespace-nowrap
-                px-6
+                px-3
+                text-[140px]
                 ${className}
+                leading-none
             `}
         >
             {text}
@@ -27,22 +31,28 @@ const Phrase = ({ text, className = "" }: PhraseProps) => {
     );
 };
 
-const ParallaxRow = ({ progress, direction, text, left }: ParallaxRowProps) => {
-    const x = useHorizontalParallax(
-        progress,
-        direction
-    );
+const Separator = () => (
+    <span className="px-6 text-white/30 text-[80px] leading-none">
+        •
+    </span>
+);
 
+const ParallaxRow = ({ progress, index, text, left }: ParallaxRowProps) => {
+    const direction = index % 2 === 0 ? "right" : "left";
+    const speeds = [1, 2.2, 1.4];
+    const x = useHorizontalParallax(progress, direction, 300, speeds[index] ?? 1);
     return (
         <motion.div
-            style={{ x, left: left }}
-            className="relative flex whitespace-nowrap"
+            style={{ x, left }}
+            className="relative flex items-center whitespace-nowrap p-4"
         >
             <Phrase text={text} />
+            <Separator />
             <Phrase text={text} />
+            <Separator />
             <Phrase text={text} />
         </motion.div>
     );
-}
+};
 
 export default ParallaxRow
